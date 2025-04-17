f2e3d3e2e2691d0d972e20dec046188f6bee2528
Andrei Corpodeanu <andrei.corpodeanu@gmail.com>
2025-04-17 21:35:23 +0300

    Initial Commit

---
+++ HyprCuts/HyprCutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyprCutsApp.swift
+//  HyprCuts
+//
+//  Created by Andrei Corpodeanu on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct HyprCutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
